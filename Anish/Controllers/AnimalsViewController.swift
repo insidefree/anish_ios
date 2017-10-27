90de9d2b5fbedc8afd1296bb9236b60d94cc0377
Denis Sorokin <denys.sorokin@gmail.com>
2017-10-27 20:58:47 +0300

    Aded styles for tableView

--- Anish/Controllers/AnimalsViewController.swift
+++ Anish/Controllers/AnimalsViewController.swift
@@ -22,11 +22,13 @@ class AnimalsViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 20
     }
 }
