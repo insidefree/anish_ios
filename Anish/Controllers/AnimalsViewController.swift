76213d772fe918235719473864094a24a2261055
Denis Sorokin <denys.sorokin@gmail.com>
2017-10-27 20:46:36 +0300

    Added table view

---
+++ Anish/Controllers/AnimalsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AnimalsViewController.swift
+//  Anish
+//
+//  Created by Denis Sorokin on 27/10/2017.
+//  Copyright © 2017 Denis Sorokin. All rights reserved.
+//
+
+import UIKit
+
+class AnimalsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
